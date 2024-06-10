2d096590a40c8cfd98c51a2fe13eb3503a044aab
Guilherme <guilhermegarcia487@gmail.com>
2024-06-09 23:12:34 -0300

    adição google font roboto, ajustes no arquivos de rotas, para adição de novos componentes, criação de estilos da nova fonte

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { InitialPageComponent } from './pages/initial-page/initial-page.component';
-import { SingUpComponent } from './pages/signup/signup.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InitialPageComponent,LoginComponent,SingUpComponent],
+  imports: [RouterOutlet, InitialPageComponent,LoginComponent,SignUpComponent,HeaderComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { InitialPageComponent } from './pages/initial-page/initial-page.component';
-import { SingUpComponent } from './pages/signup/signup.component';
+import { SignUpComponent } from './pages/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: InitialPageComponent
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', component: HeaderComponent,
+    children: [
+      {path: 'home', component: InitialPageComponent},
+    ]
   },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'signup',
-    component: SingUpComponent
-  }
+  {path: 'login', component: LoginComponent},
+  {path: 'signup',component: SignUpComponent}
 ];
